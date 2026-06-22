e6e8bb75f56a9b7d65d845726afedc958f9460a7
ebooksdeinformatica-ux <ebooksdeinformatica@gmail.com>
2026-06-22 11:25:46 -0300

    Agregar Statcounter global

---
+++ assets/js/statcounter.js
@@ -0,0 +1,11 @@
+// Statcounter global para BuenosDia.com
+var sc_project=12058975;
+var sc_invisible=1;
+var sc_security="49f17e11";
+(function(){
+  var s=document.createElement('script');
+  s.type='text/javascript';
+  s.async=true;
+  s.src='https://www.statcounter.com/counter/counter.js';
+  document.head.appendChild(s);
+})();
