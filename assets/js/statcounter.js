c77da85c0b894c46e6308ecbc5d649f305135ed0
ebooksdeinformatica-ux <ebooksdeinformatica@gmail.com>
2026-06-26 10:27:44 -0300

    Actualizar codigo Statcounter

--- assets/js/statcounter.js
+++ assets/js/statcounter.js
@@ -1,7 +1,7 @@
-// Statcounter global para BuenosDia.com
-var sc_project=12058975;
+// Statcounter global
+var sc_project=11923944;
 var sc_invisible=1;
-var sc_security="49f17e11";
+var sc_security="5b35b841";
 (function(){
   var s=document.createElement('script');
   s.type='text/javascript';
